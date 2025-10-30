3d5d14fd8261b139b128e47868fa818567367338
stachoz <stasiekzdybel9@gmail.com>
2025-10-30 16:20:18 +0100

    change names of plots

--- lab_03/analysis/results.py
+++ lab_03/analysis/results.py
@@ -8,30 +8,30 @@ dimensions = [10]
 
 results_dir = os.path.join(BASE_DIR, "..", "results")
 files = [
-    # os.path.join(results_dir, "test1-bin_10.csv"),
-    os.path.join(results_dir, "test1-real_10.csv"),
-    # os.path.join(results_dir, "test2-bin_10.csv"),
-    # os.path.join(results_dir, "test2-real_10.csv"),
+    # os.path.join(results_dir, "test1-bin_10"),
+    os.path.join(results_dir, "test1-real_10"),
+    # os.path.join(results_dir, "test2-bin_10"),
+    # os.path.join(results_dir, "test2-real_10"),
 ]
 
 for file in files:
     for n in dimensions:
         # file_path = os.path.join(BASE_DIR, "..", "results", f'test1-binary-{n}.csv')
-        df = pd.read_csv(file)
+        df = pd.read_csv(file + ".csv")
 
         evaluations = df.iloc[:, 0]
         results = df.iloc[:, 1]
 
         plt.figure(figsize=(10, 6))
         plt.plot(evaluations, results, label=f'n={n}', marker='o')
-        plt.title(f'Simualted Annealing - n={n}')
+        plt.title(f'Simualted Annealing - {file}')
         plt.xlabel('Liczba ewaluacji')
         plt.ylabel('Wynik')
         plt.legend()
         # plt.xscale('log')
         plt.grid(True)
 
-        output_file = os.path.join(BASE_DIR, f'sim_annealing_n{n}.png')
+        output_file = os.path.join(BASE_DIR, f'{file}.png')
         plt.savefig(output_file)
         plt.close()
         print(f'Wykres zapisany: {output_file}')
